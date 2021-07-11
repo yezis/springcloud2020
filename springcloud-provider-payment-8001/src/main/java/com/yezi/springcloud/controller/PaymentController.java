4d4960ec531680524a3f55ede7945c34e2919723
wangyiming <744177929@qq.com>
2021-07-11 17:48:34 +0800

    auto restart service

--- springcloud-provider-payment-8001/src/main/java/com/yezi/springcloud/controller/PaymentController.java
+++ springcloud-provider-payment-8001/src/main/java/com/yezi/springcloud/controller/PaymentController.java
@@ -31,9 +31,9 @@ public class PaymentController {
     }
 
     @GetMapping("/payment/get/{id}")
-    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
-        log.info("****查出结果：" + payment);
+        log.info("****查出结果：" + payment + "");
 
         if (payment != null) {
             return new CommonResult(200, "查询数据成功", payment);
